59ebdef38d3ed6fb2fc3b60cb50c811bf72af06e
siddharth <Sid@study.com>
2022-11-04 19:19:24 +0530

    creating nav

---
+++ todo-ninja/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav>
+        <v-toolbar elevation="4" flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer=!drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span class="font-weight-dark">Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-navigation-drawer  app v-model="drawer" class="indigo">
+            <p>test</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            drawer:false
+        }}
+    }
+</script>
